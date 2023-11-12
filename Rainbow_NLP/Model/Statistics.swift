2592498a7f6de0fc263989e66f2434bd15a2a28b
pashaGriDev <104725088+pashaGriDev@users.noreply.github.com>
2023-11-12 22:05:26 +0400

    added UserSetting and Statistics model

---
+++ Rainbow_NLP/Model/Statistics.swift
@@ -0,0 +1,12 @@
+//
+//  Statistics.swift
+//  Rainbow_NLP
+//
+//  Created by Павел Грицков on 12.11.23.
+//
+
+import Foundation
+
+struct Statistics {
+    
+}
---
+++ Rainbow_NLP/Model/UserSetting.swift
@@ -0,0 +1,12 @@
+//
+//  UserSetting.swift
+//  Rainbow_NLP
+//
+//  Created by Павел Грицков on 12.11.23.
+//
+
+import Foundation
+
+struct UserSetting {
+    
+}
