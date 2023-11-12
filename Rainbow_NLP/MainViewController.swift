c68d68b0a1de4ee5f04fe21747169b9b8f9fd701
pashaGriDev <104725088+pashaGriDev@users.noreply.github.com>
2023-11-12 21:41:16 +0400

    delete storyboard, add mainVC

--- Rainbow_NLP/MainViewController.swift
+++ Rainbow_NLP/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  Rainbow_NLP
 //
 //  Created by Павел Грицков on 12.11.23.
@@ -7,11 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-    }
-}
 
+        // Do any additional setup after loading the view.
+    }
+
+}
