a21a88f660c83fc812679fbfc19b52cc7e690b15
Alexander Altman <qewhouse@gmail.com>
2023-11-12 20:47:49 +0300

    add Settings VC

---
+++ Rainbow_NLP/SettingsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SettingsViewController.swift
+//  Rainbow_NLP
+//
+//  Created by Alexander Altman on 12.11.2023.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
