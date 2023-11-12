3f120da71f2740594fa5a88d91a0c9954a3e2d82
pashaGriDev <104725088+pashaGriDev@users.noreply.github.com>
2023-11-12 18:21:04 +0400

    Initial Commit

---
+++ Rainbow_NLP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rainbow_NLP
+//
+//  Created by Павел Грицков on 12.11.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
