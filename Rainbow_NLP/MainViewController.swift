37cb412c5301c269fe0ba73e930cf81e32d2f332
pashaGriDev <104725088+pashaGriDev@users.noreply.github.com>
2023-11-15 20:39:03 +0400

    added MainVC and MainView, add fonts files

--- Rainbow_NLP/MainViewController.swift
+++ Rainbow_NLP/MainViewController.swift
@@ -7,17 +7,53 @@
 
 import UIKit
 
+
 class MainViewController: UIViewController {
     
     let mainView = MainView()
     
+
+
+    @objc func newGameButtonTapped () {
+        print ("Go to new game")
+        
+    }
+    @objc func rulesButtonTapped (_ sender: UIButton) {
+        print ("Go to rules")
+    }
+    @objc func settingsButtonTapped (_ sender: UIButton) {
+        print ("Go to settings")
+    }
+    @objc func statsButtonTapped (_ sender: UIButton) {
+        print ("Go to stats")
+    }
+    
     override func loadView() {
         super.loadView()
         view = mainView
+                
     }
-
     override func viewDidLoad() {
         super.viewDidLoad()
+    
+        
+        
+        
+        
+        
+       
+        
+        // радуга картинка расположение
+        
+        
+        // на завтра
+        // разобраться с MainView и MainViewController
+        // разобраться с расстоянием между леблами
+        // выянить почему дебаггер ругается на констрейнты
+        
+        
+     
     }
 
 }
+
