e94657962a0d95f136711103305934fd81579381
Mark Raitman <markraitman@mail.ru>
2023-11-18 18:35:40 +0300

    Main screen buttons' constraints
    Main screen buttons' constraints problem fixed and new continueButton added

--- Rainbow_NLP/MainModule/MainViewController.swift
+++ Rainbow_NLP/MainModule/MainViewController.swift
@@ -27,6 +27,7 @@ class MainViewController: UIViewController {
         mainView.newGameButton.addTarget(self, action: #selector(newGameButtonTapped), for: .touchUpInside)
         mainView.resultButton.addTarget(self, action: #selector(resultButtonTapped), for: .touchUpInside)
         mainView.settingButton.addTarget(self, action: #selector(settingsButtonTapped), for: .touchUpInside)
+        mainView.continueButton.addTarget(self, action: #selector(continueButtonTapped), for: .touchUpInside)
         mainView.rulesButton.addTarget(self, action: #selector(rulesButtonTapped), for: .touchUpInside)
     }
 }
@@ -46,6 +47,12 @@ class MainViewController: UIViewController {
     func settingsButtonTapped(_ sender: UIButton) {
         let vc = SettingsViewController()
         navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    func continueButtonTapped(_ sender: UIButton) {
+        let vc = GameViewController()
+        navigationController?.pushViewController(vc, animated: true)
+        print ("Go back to game")
     }
     
     func resultButtonTapped(_ sender: UIButton) {
