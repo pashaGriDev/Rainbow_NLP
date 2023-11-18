9152e7b0008438f3a7c945f3557e7104fb9ed703
pashaGriDev <104725088+pashaGriDev@users.noreply.github.com>
2023-11-18 19:28:59 +0400

    added userSettingModel and update game module

--- Rainbow_NLP/GameModule/GameViewController.swift
+++ Rainbow_NLP/GameModule/GameViewController.swift
@@ -7,28 +7,36 @@
 
 import UIKit
 
+struct UserSetting: Codable {
+    let gameTime: Int
+    ///  продолжительность отображения ячейки
+    let durationTime: Int
+    let isBackground: Bool
+    
+    init(
+        gameTime: Int = 30,
+        durationTime: Int = 3,
+        isBackground: Bool = true
+    ) {
+        self.gameTime = gameTime
+        self.durationTime = durationTime
+        self.isBackground = isBackground
+    }
+}
+
 class GameViewController: BaseViewController {
-    // MARK: - Dependencies
+    // MARK: - Private properties
+    private var userSetting: UserSetting = .init()
+    private let dataManager: DataManager = DataManager<UserSetting>()
+    
+    private var isGameRunning: Bool = true
     private var timer: Timer?
     private var collectionView: UICollectionView!
     private let gamePlay = GamePlayModel()
     private var gameTime = 15
-    var isGameRunning: Bool = true
-    var gameWithBG: Bool = false
     
-    private var visualEffectView: UIVisualEffectView!
+    private var visualEffectView: UIVisualEffectView = VisualEffectView(effect: UIBlurEffect(style: .light))
     private var isVisualEffectViewHidden = true
-    
-    private lazy var pauseLabel: UILabel = {
-        let element = UILabel()
-        element.text = "Pause"
-        element.textAlignment = .center
-        element.font = .boldSystemFont(ofSize: 50)
-        element.textColor = .blue
-        element.makeShadow()
-        element.translatesAutoresizingMaskIntoConstraints = false
-        return element
-    }()
     
 //    private lazy var bgrndImageView: UIImageView = {
 //        let element = UIImageView()
@@ -39,12 +47,14 @@ class GameViewController: BaseViewController {
 //        return element
 //    }()
     
+    
     // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         initialStateView()
         setupCollectionView()
         createVisualEffect()
+        setUserSetting()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -54,10 +64,20 @@ class GameViewController: BaseViewController {
     
     //MARK: - Methods
     private func initialStateView() {
-        setNavigationTitle = "Preparing"
+        setNavigationTitle = "Preparing..."
 //        view.addSubview(bgrndImageView)
 //        bgrndImageView.pinToNav(to: view, nav: navigationBarView)
     }
+    
+    private func setUserSetting() {
+        do {
+            let userSetting = try dataManager.load(by: .userSettingData)
+            self.userSetting = userSetting
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
     
     override func backButtonAction(_ sender: UIButton) {
         super.backButtonAction(sender)
@@ -93,24 +113,8 @@ class GameViewController: BaseViewController {
     
     //MARK: - Visual Blur Effect View
     private func createVisualEffect() {
-        let blurEffect = UIBlurEffect(style: .light)
-        visualEffectView = UIVisualEffectView(effect: blurEffect)
-        visualEffectView.translatesAutoresizingMaskIntoConstraints = false
-        visualEffectView.contentView.addSubview(pauseLabel)
         view.addSubview(visualEffectView)
-        visualEffectView.isHidden = true
-        NSLayoutConstraint.activate([
-            
-            visualEffectView.topAnchor.constraint(equalTo: navigationBarView.bottomAnchor),
-            visualEffectView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            visualEffectView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            visualEffectView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            
-            pauseLabel.centerXAnchor.constraint(equalTo: visualEffectView.centerXAnchor),
-            pauseLabel.centerYAnchor.constraint(equalTo: visualEffectView.centerYAnchor),
-            pauseLabel.widthAnchor.constraint(equalTo: visualEffectView.widthAnchor, multiplier: 0.5),
-            pauseLabel.heightAnchor.constraint(equalToConstant: 50)
-        ])
+        visualEffectView.frame = collectionView.frame
     }
     
     private func toggleVisualEffectView() {
@@ -190,33 +194,42 @@ extension GameViewController: UICollectionViewDataSource {
         1
     }
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath
+    ) -> UICollectionViewCell {
+        
         let cell = collectionView.dequeueReusableCell(
             withReuseIdentifier: String(describing: CustomCell.self),
             for: indexPath
         ) as! CustomCell
-        //        let config = gamePlay.getConfig()
         
-        if gameWithBG {
+        switch userSetting.isBackground {
+        case true:
             let config = gamePlay.getConfig()
             cell.configure(text: config.text, and: config.color)
-        } else {
+        case false:
             let config = gamePlay.getConfigNoBg()
             cell.configure(text: config.text, and: config.color)
         }
         
-//        cell.configure(text: config.text, and: config.color)
         return cell
     }
 }
 
 //MARK: - UICollectionViewDelegateFlowLayout
 extension GameViewController: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        insetForSectionAt section: Int
+    ) -> UIEdgeInsets {
+        
         let width = self.collectionView.bounds.width - 150
         let height = self.collectionView.bounds.height - 50
         let top: CGFloat = Double.random(in: 0...height)
         let left: CGFloat = Double.random(in: 0...width)
+        
         return .init(top: top, left: left, bottom: 0, right: 0)
     }
 }
